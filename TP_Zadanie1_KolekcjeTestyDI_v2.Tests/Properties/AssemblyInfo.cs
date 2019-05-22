066ce2ef9877962ad907736321538fa625ecc68f
Vladyslav Andriienko <187193@edu.p.lodz.pl>
2019-05-22 23:12:00 +0200

    Добавьте файлы проекта.

---
+++ TP_Zadanie1_KolekcjeTestyDI_v2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TP_Zadanie1_KolekcjeTestyDI_v2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TP_Zadanie1_KolekcjeTestyDI_v2.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3197b0f-ed67-4263-b657-3c357bd9c9ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
