70bfce65977e24010edaa2910b91b80a53e68848
Vladyslav Andriienko <187193@edu.p.lodz.pl>
2019-05-23 00:18:15 +0200

    [TP-Zadanie 1 - Częćś 1d] Dodana klasa zarządzającą obiektami danych

---
+++ TP_Zadanie1_KolekcjeTestyDI_v2/DataRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+using TP_Zadanie1_KolekcjeTestyDI;
+
+namespace TP_Zadanie1_KolekcjeTestyDI_v2
+{
+    class DataRepository
+    {
+        private DataContext dataContext;
+    }
+}
